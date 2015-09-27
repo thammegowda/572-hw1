3b22e8d15bd43c1c249a73e8ba82bbf2caedb88a
tg <tnarayan@usc.edu>
2015-09-27 16:17:37 -0700

    Added Java script to test site pages

---
+++ test-site/js/extjs.js
@@ -0,0 +1,5 @@
+console.log("Ext page Javascript was run")
+var mydiv = document.getElementById("jspad")
+var imgTag = document.createElement('img');
+imgTag.setAttribute('src',"images/ext-js-image.png");
+mydiv.appendChild(imgTag);
