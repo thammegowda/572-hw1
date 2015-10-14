c096f0e1f1454c8c1bd86863f26c1e7cd5ad5308
tg <tnarayan@usc.edu>
2015-10-14 14:39:34 -0700

    add argument parser for CLI args

--- reader-py/nutch_reader.py
+++ reader-py/nutch_reader.py
@@ -8,33 +8,56 @@
 #  Nii Mante
 
 
-
 ## CONFIG BLOCK BEGIN
-
-SEGMENTS_CONTENT_DIR = "/home/tg/work/coursework/cs572/hw1/sites/armslist/c1/merged/*/content/"
-CRAWLDB_DIR = "/home/tg/work/coursework/cs572/hw1/sites/armslist/c1/crawldb/current/"
-GROUP_NAME = "USC CSCI-572 FALL-2015 GROUP-36; ThammeGowda, Rakshith, Rahul and Nii"
-
+__author__ = "USC CSCI-572 FALL-2015 GROUP-36; ThammeGowda, Rakshith, Rahul and Nii"
 ## CONFIG BLOCK END
 
 
 from nutchpy import sequence_reader as seq_reader
 import os
+import argparse
+import sys
 
-def scan_mime_types(segment_paths, limit=None):
+def scan_mime_types(crawldb):
     '''
     scans all mime types from given segments
+    'param: path to crawldb
     :return: key of mime types
     '''
     count = 0
     mimes = {}
-    for k,doc in seq_reader.read_all(segment_paths, limit=limit):
-        #print "# %d :: %s :: %s" % (count, doc.get("contentType"), k)
+    mime_key = 'Content-Type'
+    for url,doc in seq_reader.read_all(crawldb):
+        #print "# %d :: %s :: %s" % (count, doc.get(mime_key), k)
+
+        # the conversion of sequence file record to python dictionary sometimes fails
+        # So defensive programming in action
+    
+        for k in doc.keys():
+            if k and k.startswith(mime_key):
+                #print "%s :: %s" %(k, url)
+                mime_val = None
+                if '=' in k:
+                    # then it is of form Content-Type=image/jpeg
+                    mime_val = k.split('=')[-1]
+                elif k == mime_key:
+                    #then the record was properly converted to dictionary
+                    mime_val = doc.get(k)
+
+                if mime_val:
+                    mime_val = mime_val.strip()
+                    if mime_val:
+                        #mime val is found, lets update count
+                        mimes[mime_val] = mimes.get(mime_val, 0) + 1
+                        #print "%s::%s" %(mime_val, url)
+                #already found the mimetype of this doc, break the loop        
+                break
+
         count += 1
-        mimes[doc.get('contentType')] = mimes.get(doc.get('contentType', 'unknown/unknown'), 0) + 1
+        if count % 1000 == 0:
+            print count
     mimes["__total__"] = count
     return mimes
-
 
 def find_sequence_files(path):
 
@@ -43,10 +66,9 @@ def find_sequence_files(path):
     :param path: the root path
     :return: list of paths
     """
-
     abs_path = os.path.abspath(path)
     #WARNING: this works only in Unix environment
-    find_cmd = "find %s -maxdepth 10 -type f -regex .*part-[0-9]*/data" % abs_path
+    find_cmd = "find %s -maxdepth 5 -type f -regex .*part-[0-9]*/data" % abs_path
     import subprocess
     out = subprocess.Popen(find_cmd, shell=True, stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -58,58 +80,86 @@ def find_sequence_files(path):
 
 # task 2c
 # Images Mimes
-def get_image_mimes():
+def get_mimes(segments_path, mime_prefix="image"):
+    '''
+    Scans segment content for detecting content type
+    :param : segments_path - path to segments. This could be regex to match all segment directories
+    '''
     # We scan the content directory
-    # Scan crawl db for faster and more images
-    files = find_sequence_files(SEGMENTS_CONTENT_DIR)
-    mimes = scan_mime_types(files, 1000)
+    files = find_sequence_files(segments_path)
+    mimes = scan_mime_types(files)
     img_mimes = {}
     for mime, count in mimes.items():
-        mime = mime.strip().lower()
-        if mime.startswith("image"):
-            img_mimes[mime] = count
+        if mime:
+            mime = mime.strip().lower()
+            if mime.startswith(mime_prefix):
+                img_mimes[mime] = count
+        else:
+            print "Error Mime:Count= %s:%s" %(mime,count)
     return img_mimes
 
 
 # task 2d, failed urls
-def get_failed_urls(n=None):
+def get_failed_urls(crawldb, n=None):
     """
-        Run this command in bash
-        nutch readdb try1/crawldb/ -dump ~/tmp/crawldb -status db_gone -format csv
+    Gets failure urls from crawldb
+    same as nutch command
+    ># nutch readdb try1/crawldb/ -dump ~/tmp/crawldb -status db_gone -format csv
+    :param : crawldb - path to crawl db
+    :param : n - number of failed urls to get. 
     """
-    files = find_sequence_files(CRAWLDB_DIR)
+    files = find_sequence_files(crawldb)
     recs = seq_reader.read_all(files)
     failure_statuses = {'3 (db_gone)'}
     count = 0
-    for k,v in recs:
+    for k, v in recs:
         if 'Status' in v:
             status = v.get('Status').strip()
             if status in failure_statuses:
                 count += 1
                 yield k.toString(), status
+                if n and count >= n:
+                    return
         else:
             print "No Status for %s" % (k)
-        if n and count > n:
-            return
 
 
-def create_report(bad_urls_file, mime_types_file):
+def create_report(args):
     '''
     Creates report files
-    :param bad_urls_file: path to file where failed urls needs to be saved
-    :param mime_types_file: path to file where image mimes needs to be saved
-    :return:
+    :return: None
     '''
-    with open(mime_types_file, 'w') as outf:
-        outf.write("#2c Image MIMES \n")
-        for mime_type,count in get_image_mimes().items():
-            outf.write("%s, %d\n" %(mime_type, count))
-
-    with open(bad_urls_file, 'w') as outf:
-        outf.write("#2d 100 failed urls\n")
-        for url,status in get_failed_urls(n=110):
-            outf.write("%s, %s\n" %(url, status))
-
+    cmd = args.get('command')
+    if cmd == 'failed':
+        with open(args['report'], 'w') as outf:
+            n = args.get('num_failures')
+            outf.write("# %s \n" % __author__)
+            outf.write("## 2d. First %d failed urls\n" % n)
+            for url,status in get_failed_urls(args['crawldb'], n=n):
+                outf.write("%s, %s\n" %(url, status))
+                
+    elif cmd == 'mimes':
+        with open(args['report'], 'w') as outf:
+            outf.write("# %s \n" % __author__)
+            outf.write("## 2c. Image MIMES \n")
+            for mime_type,count in get_mimes(args['crawldb']).items():
+                outf.write("%s, %d\n" %(mime_type, count))
+    else:
+        print "ERROR: Unknown Command : %s" % cmd
 
 if __name__ == '__main__':
-    create_report("bad_urls.txt", "image_mimes.txt")
+
+    parser = argparse.ArgumentParser(description="Crawl Stats Reader")
+
+    subparsers = parser.add_subparsers(help ="sub-commands", dest="command")
+    fail_parser = subparsers.add_parser("failed", help="dumps failed urls")
+    fail_parser.add_argument("-n", "--num-failures", help="Number of failure URLS", default=100, type=int)
+    fail_parser.add_argument("-r", "--report", help="Path to report File", required=True)
+    fail_parser.add_argument("-db", "--crawldb", help="Path to crawldbs. RegEx is also accepted", required=True)
+
+    mime_parser = subparsers.add_parser("mimes", help="Dumps Image Mimes")
+    mime_parser.add_argument("-r", "--report", help="Path to report File", required=True)
+    mime_parser.add_argument("-db", "--crawldb", help="Path to crawldbs. RegEx is also accepted", required=True)
+    
+    args = vars(parser.parse_args(sys.argv[1:]))
+    create_report(args)
